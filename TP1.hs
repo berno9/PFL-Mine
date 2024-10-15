dcc5daf683e223dcad9248bcf2a3efe27b384207
Rei <bernardo2004costa@gmail.com>
2024-10-15 18:07:19 +0100

    First four functions working correctly.

---
+++ TP1.hs
@@ -0,0 +1,73 @@
+import qualified Data.List
+import Data.List (nub)
+import qualified Data.Array
+import qualified Data.Bits
+
+-- PFL 2024/2025 Practical assignment 1
+
+-- Uncomment the some/all of the first three lines to import the modules, do not change the code of these lines.
+
+type City = String
+type Path = [City]
+type Distance = Int
+
+type RoadMap = [(City,City,Distance)]
+
+cities :: RoadMap -> [City]
+cities roadMap = nub [city | (city1, city2, _) <- roadMap, city <- [city1, city2]]
+
+-- go through each tuple checking if the cities are the ones we're looking for
+areAdjacent :: RoadMap -> City -> City -> Bool
+areAdjacent roadMap city1 city2 =
+    any (\(cityA, cityB, _) -> (city1 == cityA && city2 == cityB) || (city1 == cityB && city2 == cityA)) roadMap    
+
+-- recursively check each tuple to find both cities, and return associated distance
+--distance :: RoadMap -> City -> City -> Maybe Distance
+--distance roadMap cityA cityB =
+--   case lookupDist roadMap cityA cityB of
+--        Just d -> Just d
+--        Nothing -> lookupDist roadMap cityB cityA
+--    where
+--        lookupDist :: RoadMap -> City -> City -> Maybe Distance
+--        lookupDist [] _ _ = Nothing
+--        lookupDist ((c1, c2, dist):rest) city1 city2
+--            | city1 == c1 && city2 == c2 = Just d
+--            | otherwise                  = lookupDist rest city1 city2
+distance :: RoadMap -> City -> City -> Maybe Distance
+distance roadMap city1 city2 = 
+    case [(d) | (c1, c2, d) <- roadMap, (c1 == city1 && c2 == city2) || (c1 == city2 && c2 == city1)] of
+        [d] -> Just d
+        _   -> Nothing
+
+
+adjacent :: RoadMap -> City -> [(City,Distance)]
+adjacent roadMap city = [(c2, d) | (c1, c2, d) <- roadMap, c1 == city] ++ [(c1, d) | (c1, c2, d) <-roadMap, c2 == city]
+
+
+pathDistance :: RoadMap -> Path -> Maybe Distance
+pathDistance = undefined
+
+rome :: RoadMap -> [City]
+rome = undefined
+
+isStronglyConnected :: RoadMap -> Bool
+isStronglyConnected = undefined
+
+shortestPath :: RoadMap -> City -> City -> [Path]
+shortestPath = undefined
+
+travelSales :: RoadMap -> Path
+travelSales = undefined
+
+tspBruteForce :: RoadMap -> Path
+tspBruteForce = undefined -- only for groups of 3 people; groups of 2 people: do not edit this function
+
+-- Some graphs to test your work
+gTest1 :: RoadMap
+gTest1 = [("7","6",1),("8","2",2),("6","5",2),("0","1",4),("2","5",4),("8","6",6),("2","3",7),("7","8",7),("0","7",8),("1","2",8),("3","4",9),("5","4",10),("1","7",11),("3","5",14)]
+
+gTest2 :: RoadMap
+gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2","3",30)]
+
+gTest3 :: RoadMap -- unconnected graph
+gTest3 = [("0","1",4),("2","3",2)]
